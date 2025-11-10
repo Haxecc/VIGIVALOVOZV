785b02f40c5a146ccea548be1e12ed7e03f3896e
Haxecc <antonmyashinqaz@gmail.com>
2025-11-10 20:44:12 +0300

    brand new way

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,36 @@
 
 int main(void)
 {
-  sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+  unsigned int HEIGHT = 800;
+  unsigned int WIDTH  = 600;
+  
+  sf::RenderWindow window(sf::VideoMode({HEIGHT, WIDTH}), "VIZIVALOVOZV", sf::Style::None, sf::State::Windowed); // 4-th argument will be sf::State::Fullscreen
+  window.setFramerateLimit(60);
+
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
 
+  const auto onClose = [&window](const sf::Event::Closed&)
+  {
+    window.close(); 
+  };
+
+  const auto onKeyPressed = [&window](const sf::Event::KeyPressed& keyPressed)
+  {
+    if (keyPressed.scancode == sf::Keyboard::Scancode::Escape)
+      window.close();
+  };
+
   while (window.isOpen())
   {
-    while (const std::optional event = window.pollEvent())
-    {
-      if (event->is<sf::Event::Closed>())
-        window.close();
-    }
-  
-  window.clear();
-  window.draw(shape);
-  window.display();
+    window.handleEvents(onClose, onKeyPressed); 
 
+    window.clear();
+    window.draw(shape);
+    window.display();
   }
+
+  return 0;
 }
+
+
