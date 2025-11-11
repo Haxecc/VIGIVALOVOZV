9f2f0a7c4a93f55ba5a399c4d2f61caf84a36487
Haxecc <antonmyashinqaz@gmail.com>
2025-11-11 19:04:26 +0300

    fixed install script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 echo "START BUILDING"
-cmake --build build/
-mv build/VISIVALOVOZV .
+mkdir build
+cd build
+cmake ..
+cmake --build .
+mv VISIVALOVOZV ..
 echo -e "\033[0;32mBUILDING DONE\033[0m"
